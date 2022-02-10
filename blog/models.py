5f47fec6b5dc77626421581953d05352ad3b4435
RedbinLee <redbinlee@gmail.com>
2022-02-10 19:32:26 +0900

    create a model, ForeignKey requires an argument, on_delete

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
