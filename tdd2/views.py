563184919714dcaef167f636f058a0089317a745
RedbinLee <redbinlee@gmail.com>
2022-02-14 13:22:29 +0900

    test if the blog entries show up on the homepage, using ListView instead of TemplateView in views.py in project, index extends base

--- tdd2/views.py
+++ tdd2/views.py
@@ -1,7 +1,13 @@
 # index view. Not blog application view.
 
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from blog.models import Entry
+
+# TemplateView needs to be ListView 
+# from django.views.generic.base import TemplateView
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'index.html'
+    queryset = Entry.objects.order_by('-created_at')
