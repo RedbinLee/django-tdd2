baba032eb7484db4e7c0e2283c59269150592a47
RedbinLee <redbinlee@gmail.com>
2022-02-15 19:13:27 +0900

    add comment class and test comment body == __str__ of Comment

--- blog/models.py
+++ blog/models.py
@@ -25,3 +25,15 @@ class Entry(models.Model):
     class Meta:
         # this will be tested under the _meta class in test.py
         verbose_name_plural = "entries"
+
+class Comment(models.Model) :
+    entry = models.ForeignKey(Entry, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    
+    def __str__(self) :
+        return self.body
